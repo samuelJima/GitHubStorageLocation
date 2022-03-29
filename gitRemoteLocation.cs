4e798b0f0c455b8835145ae842e1a074ca5bbaaa
samuel <assefajima@gmail.com>
2022-03-29 14:37:51 +0300

    remote location Branch update

--- gitRemoteLocation.cs
+++ gitRemoteLocation.cs
@@ -4,4 +4,8 @@ public GitRemoteLocation(){
   remote_location= "this is remote location";
 }
   private string remote_location {get; set}
+
+  private string GetRemotelocation(){
+    return remote_location;
+  }
 }
